7ed75db708f6f6abf4b84e552c356a73f13f3df7
Ricky <iarexd@gmail.com>
2022-01-01 13:56:49 -0500

    fix: deploy scripts for voting system

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var VotingSystem = artifacts.require("./VotingSystem.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = function (deployer) {
+    deployer.deploy(VotingSystem);
 };
